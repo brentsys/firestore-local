00a0eb345bfc312d58eec524db208bd9a201b444
Henri Sack <dev@brentsys.com>
2019-04-09 11:30:57 +0100

    eported describer

--- index.ts
+++ index.ts
@@ -2,4 +2,5 @@ export * from "./lib/firebase"
 export * from "./lib/counter"
 export * from "./lib/record_model"
 export * from "./lib/record_type"
+export * from "./lib/describer"
 export { Fixture } from './lib/local-firestore';
