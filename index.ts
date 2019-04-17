f945032b35a115f75c66905c27303070720171d9
Henri Sack <dev@brentsys.com>
2019-04-17 16:01:10 +0100

    exporteed TestDB class

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@ export * from "./lib/record_model"
 export * from "./lib/record_type"
 export * from "./lib/describer"
 export * from "./lib/firestore_upload"
+export * from "./db/test_db"
 export { Fixture } from './lib/local-firestore';
